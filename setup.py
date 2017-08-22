552269305d5979b98c7acd2bc6577d0bb942a2e6
Lachlan Gunn <lachlan.gunn@unisa.edu.au>
2017-08-22 15:41:03 +0930

    Remove tensorflow-gpu dependency.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     ],
     keywords='deeplearning neuralnetworks',
     packages=find_packages(),
-    install_requires=['flask', 'keras>=2.0.6', 'tensorflow-gpu',  'jsonschema', 'numpy', 'requests',
+    install_requires=['flask', 'keras>=2.0.6', 'tensorflow',  'jsonschema', 'numpy', 'requests',
                             'requests-toolbelt', 'tqdm'],
     python_requires='>=3',
 
