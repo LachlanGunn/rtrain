b4a6f89a8b147005ea01da09e5e7c588b42d8702
Lachlan Gunn <lachlan.gunn@unisa.edu.au>
2017-08-30 15:27:58 +0930

    Remove commented code.

--- rtrain/server.py
+++ rtrain/server.py
@@ -237,24 +237,6 @@ def main():
         help='Path to configuration file.')
     args = parser.parse_args()
 
-    # logging.basicConfig(stream=sys.stdout)
-    # structlog.configure(
-    #     processors=[
-    #         structlog.stdlib.filter_by_level,
-    #         structlog.stdlib.add_logger_name,
-    #         structlog.stdlib.add_log_level,
-    #         structlog.stdlib.PositionalArgumentsFormatter(),
-    #         structlog.processors.StackInfoRenderer(),
-    #         structlog.processors.format_exc_info,
-    #         structlog.processors.UnicodeDecoder(),
-    #         structlog.stdlib.render_to_log_kwargs,
-    #     ],
-    #     context_class=dict,
-    #     logger_factory=structlog.stdlib.LoggerFactory(),
-    #     wrapper_class=structlog.stdlib.BoundLogger,
-    #     cache_logger_on_first_use=True,
-    # )
-
     log = logger.new()
 
     try:
